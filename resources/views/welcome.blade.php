51210ded5fe2126ca859d8cf1311c5d5a07dfcca
kevins <kevins.choque@gmail.com>
2019-06-21 20:59:11 -0500

    primer commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,173 @@
+{{-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="{{ asset('css/app.css') }}" type="text/css" rel="stylesheet" />
+        <meta name="csrf-token" value="{{ csrf_token() }}" />
+</head>
+<body>
+    <div id="app">
+        <example-component></example-component>
+    </div>
+    <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+</body>
+</html> --}}
+
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <title>AdminLTE 3 | Starter</title>
+  <meta name="csrf-token" value="{{ csrf_token() }}" />
+{{-- <link rel="stylesheet" href="/css/app.css"> --}}
+<link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
+{{-- <script src="/js/app.js"></script> --}}
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper" id="app">
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light border-bottom">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
+
+    <!-- SEARCH FORM -->
+    <div class="input-group input-group-sm">
+      <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+      <div class="input-group-append">
+        <button class="btn btn-navbar">
+          <i class="fa fa-search"></i>
+        </button>
+      </div>
+    </div>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="{{asset('img/img2.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light">AdminLTE 3</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="{{asset('img/img1.png')}}" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Alexander Pierce</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+                <li class="nav-item">
+                    <a to="/EjercicioLima" class="nav-link">
+                        <i class="fas fa-tachometer-alt nav-icon blue"></i>
+                        <p>Ejercicio Lima</p>
+                    </a>
+                </li>
+            <li class="nav-item has-treeview ">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-cog green"></i>
+                    <p>Management<i class="right fas fa-angle-left"></i></p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a to="/users" class="nav-link">
+                            <i class="nav-icon fas fa-users"></i>
+                            <p>Users</p>
+                        </a>
+                    </li>
+                    {{-- <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-cog"></i>
+                            <p>new 2</p>
+                        </a>
+                    </li> --}}
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a to="/dashboard" class="nav-link">
+                    <i class="fas fa-tachometer-alt nav-icon blue"></i>
+                    <p>Dashboard</p>
+                </a>
+            </li>
+            <li class="nav-item">
+              <a to="/developer" class="nav-link">
+                  <i class="fas fa-cogs nav-icon"></i>
+                  <p>developers</p>
+              </a>
+            </li>
+            <li class="nav-item">
+                <a to="/profile" class="nav-link">
+                    <i class="fas fa-user-tie nav-icon orange"></i>
+                    <p>Profile</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-th red"></i>
+                    <p>log out<span class="right badge badge-danger">New</span></p>
+                </a>
+            </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        
+        {{-- <router-view></router-view> --}}
+        {{-- <vue-progress-bar></vue-progress-bar> --}}
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2014-2018 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
+{{-- <script src="/js/app.js"></script> --}}
+<script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+</body>
+</html>
